f846decf200c02b1d7d22d53f7ec009eae078783
Allen Stevens <allen.stevens@students.dominican.edu>
2024-05-21 14:50:08 -0700

    completed challenge 4

--- src/index.js
+++ src/index.js
@@ -20,3 +20,34 @@ const allCaps = (str) => {
 // Test cases
 console.log(allCaps('hello world'));
 console.log(allCaps('allen stevens'));
+
+// Challenge 3
+// capitalizeWords() - makes the first character of each word uppercase. Imagine that each word is separated by a space.
+
+// Example: do all the things -> Do All The Things
+
+const capitalizeWords = (str) => {
+    let phrase = str.split(' ');
+    let capitalizeWords = phrase.map(word => capitalize(word));
+    return capitalizeWords.join(' ');
+}
+
+// Test cases
+console.log(capitalizeWords('i love coding'));
+console.log(capitalizeWords('my name is allen'));
+
+// Challenge 4
+// removeExtraSpaces() - Removes all spaces from the beginning and end of a 
+// String along with any extra spaces in the middle. If more than one space appears in the middle of a string it is replaced by a single space.
+const removeExtraSpaces = (str) => {
+    str = str.trim();
+    str = str.split(' ').filter(word => word !== '');
+    return str.join(' ');
+}
+
+console.log(removeExtraSpaces('   i   love    coding    '));
+
+// Challenge 5
+// kebobCase() - Removes extra spaces and replaces spaces with the hyphen "-", and makes all characters lowercase.
+// Example: "   Hello    world   " -> "hello-world"
+

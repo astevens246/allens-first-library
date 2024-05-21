98fd5953363b08f1b14e8f993324f7dbb8aa7ef4
Allen Stevens <allen.stevens@students.dominican.edu>
2024-05-21 14:19:32 -0700

    completed challenge 1

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// Challenge 1
+// capitalize() - makes the first character of a given string uppercase.
+// Ex. capitalize('hello') => 'Hello'
+
+const capitalize = (str) => {
+    return str[0].toUpperCase() + str.slice(1);
+}
+// Test cases
+console.log(capitalize('hello'));
+console.log(capitalize('allen'));
+
+// Challenge 2
+// allCaps() - makes all characters uppercase. (this is the same as str.toUpperCase())
+
+// Example: foo bar -> FOO BAR
